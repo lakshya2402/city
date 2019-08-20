0325c120bdcec44e58fac91dd761a39d7974a416
lakshya2402 <lakshya.yadav@spandit.org>
2019-08-20 16:27:33 +0530

    python manage.py makemigrations data python manage.py sqlmigrate data 0001 python3 manage.py migrate

--- data/models.py
+++ data/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# connecting store and storedata so that the can connect with database
+class store(models.Model):
+    store_name= models.CharField(max_length=250)
+    store_address= models.CharField(max_length=250)
+    store_contact_no= models.IntegerField(max_length=9)
+
+class storedata(models.Model):
+    store_location= models.ForeignKey(store, on_delete=models.CASCADE)
--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+#this is our app1 data
+    'data.apps.DataConfig',
+#preinstalled
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
