e0e55d4422a0a2ba6a41cc7120f1ed8af273b7e5
lakshya2402 <lakshya.yadav@spandit.org>
2019-08-20 20:21:54 +0530

    admin pannel

--- data/admin.py
+++ data/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+
+# adding store values to admin pannel
+from .models import store
 # Register your models here.
+admin.site.register(store)
